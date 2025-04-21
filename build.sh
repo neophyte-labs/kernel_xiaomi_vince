8db97be1e0bc5d05ca40a3b4048c24fb0a5224fa
kurumich4n <alprjkt@gmail.com>
2025-04-21 18:43:14 +0000

    build.sh: clean up

--- build.sh
+++ build.sh
@@ -5,18 +5,15 @@
 
 SECONDS=0 # builtin bash timer
 ZIPNAME="Neophyte-Kernel-Vince-KSU-$(TZ=Asia/Kolkata date +"%Y%m%d-%H%M").zip"
-TC_DIR="$HOME/tc/clang"
+TC_DIR="/workspace/gitpod/clang"
 AK3_DIR="AnyKernel3"
 DEFCONFIG="vendor/vince_defconfig"
 export PATH="$TC_DIR/bin:$PATH"
 
-# Build Environment
-sudo apt update -y && sudo apt upgrade -y && sudo apt install nano bc bison ca-certificates curl flex gcc git libc6-dev libssl-dev openssl python-is-python3 ssh wget zip zstd sudo make clang gcc-arm-linux-gnueabi software-properties-common build-essential libarchive-tools gcc-aarch64-linux-gnu -y && sudo apt install build-essential -y && sudo apt install libssl-dev libffi-dev libncurses5-dev zlib1g zlib1g-dev libreadline-dev libbz2-dev libsqlite3-dev make gcc -y && sudo apt install pigz -y && sudo apt install python2 -y && sudo apt install python3 -y && sudo apt install cpio -y && sudo apt install lld -y && sudo apt install llvm -y
-
 # Check for essentials
 if ! [ -d "${TC_DIR}" ]; then
 echo "Clang not found! Cloning to ${TC_DIR}..."
-if ! git clone --depth=1 https://gitea.com/k4ngcaribug/aosp-clang -b main ${TC_DIR}; then
+if ! git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang -b main ${TC_DIR}; then
 echo "Cloning failed! Aborting..."
 exit 1
 fi
@@ -32,8 +29,7 @@ mkdir -p out
 make O=out ARCH=arm64 $DEFCONFIG
 
 # Build KSU-next
-curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -s next
-curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next-susfs/kernel/setup.sh" | bash -s next-susfs
+curl -LSs "https://raw.githubusercontent.com/KernelSu-Next/KernelSU-Next/next-susfs/kernel/setup.sh" | bash -s next-susfs
 
 echo -e "\nStarting compilation...\n"
 make -j$(nproc --all) CC=clang CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- O=out ARCH=arm64
